2e726148d226d6d67da2acbf073a687b8426a096
Rohit Sharma <mail2rohitsharmappn@gmail.com>
2023-07-13 17:18:46 +0530

    redux-increment

---
+++ src/reducers/upDown.js
@@ -0,0 +1,14 @@
+const inialState = 0;
+
+const changeTheNumber = (state = inialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export default changeTheNumber;
